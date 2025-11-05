6141222bea90256ae56273e24a096c761b0eb7a6
srsuki07 <oscarloyo2013@gmail.com>
2025-11-05 18:58:36 -0300

    Add files via upload

---
+++ MiChat.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from groq import Groq
+
+st.set_page_config(page_title="Mi chat de IA por Oscar Loyo", page_icon="🗣")
+st.title("El Primer ChatBot de Suki")
+
+nombre = st.text_input("¿Como te llamas?")
+if st.button("Saludar!"):
+    st.write(f"Hola {nombre}! Espero disfrutes tu estadia :)")
+
+MODELOS = ['llama-3.1-8b-instant', 'llama-3.3-70b-versatile']
+
+def configurar_pagina():
+    st.title("Mi Chat de IA por Oscar Loyo")
+    st.sidebar.title("Configuracion de la IA")
+
+    elegirModelo = st.sidebar.selectbox(
+        "Elegi un modelo",
+        options = MODELOS,
+        index = 0
+    )
+
+    return elegirModelo
+
+
+def crear_usuario_groq():
+    clave_secreta = st.secrets["CLAVE_API"]
+    return Groq(api_key=clave_secreta)
+
+def configurar_modelo(cliente, modelo, mensajeDeEntrada):
+    return cliente.chat.completions.create(
+        model = modelo,
+        messages = [{"role":"user", "content": mensajeDeEntrada}],
+        stream = True
+    )
+
+def inicializar_estado():
+    if "mensajes" not in st.session_state:
+        st.session_state.mensajes = []
+
+#Funciones agregadas en CLASE 8
+def actualizar_historial(rol, contenido, avatar):
+    st.session_state.mensajes.append({"role": rol, "content": contenido, "avatar": avatar})
+
+def mostrar_historial():
+    for mensaje in st.session_state.mensajes:
+        with st.chat_message(mensaje["role"], avatar= mensaje["avatar"]) : st.markdown(mensaje["content"])
+
+def area_chat():
+    contenedorDelChat = st.container(height=400, border= True)
+    with contenedorDelChat: mostrar_historial()
+
+
+# Clase 9 - funciones
+def generar_respuestas(chat_completo):
+    respuesta_completa = ""
+    for frase in chat_completo:
+        if frase.choices[0].delta.content:
+            respuesta_completa += frase.choices[0].delta.content
+            yield frase.choices[0].delta.content
+    return respuesta_completa
+
+def main ():
+    clienteUsuario = crear_usuario_groq()
+    inicializar_estado()
+    modelo = configurar_pagina()
+    area_chat() #Nuevo 
+    mensaje = st.chat_input("Escribi tu mensaje:")
+
+    if mensaje:
+        actualizar_historial("user", mensaje, "😁")
+        chat_completo = configurar_modelo(clienteUsuario, modelo, mensaje)
+        if chat_completo:
+                with st.chat_message("assistant"):
+                    respuesta_completa = st.write_stream(generar_respuestas(chat_completo))
+                    actualizar_historial("assistant", respuesta_completa, "🤖")
+                    st.rerun()
+
+if __name__ == "__main__":
+    main()
+
+
+
+#modelo = configurar_pagina()
+#mensaje = st.chat_input("Escribi tu mensaje:")
